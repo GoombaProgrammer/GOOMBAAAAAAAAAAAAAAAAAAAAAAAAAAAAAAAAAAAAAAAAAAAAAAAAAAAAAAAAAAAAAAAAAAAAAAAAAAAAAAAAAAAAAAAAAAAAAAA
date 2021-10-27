442722b2d411385e97ac1e657b4b8a713c034850
GOOMBAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA <84668888+GOOMBAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA@users.noreply.github.com>
2021-10-27 11:39:35 +0200

    Update goombaaaaaaaaaa.poopscript.js

--- goombaaaaaaaaaa.poopscript.js
+++ goombaaaaaaaaaa.poopscript.js
@@ -1,3 +1,6 @@
+// Credit to Goldenretriver, the creator of Oof Clicker on the google play store, for submitting this.
+// You need the poopscript JS library to use this.
+
 new PoopScriptEnv(PoopScriptEnv.removalTemplates.simpleUsage).exec(`
     ->log goombaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa;
 `);
